fb80efbca47a1d9a0bce070a4e56c46612f0b86f
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-05 22:40:37 +0300

    Add project files.

---
+++ Watercooler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rosi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            List<string[]> input = new List<string[]>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                input.Add(Console.ReadLine().Split(' '));
+
+            }
+            foreach (var testCase in input)
+            {
+                int rent = int.Parse(testCase[0]);
+                int buy = int.Parse(testCase[1]);
+                int months = int.Parse(testCase[2]);
+
+
+                if (rent * months < buy)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+
+
+            }
+
+
+        }
+    }
+}
